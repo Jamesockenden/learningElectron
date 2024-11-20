8e6bd7ac7ff640da50bc789a728ec46f7b508daa
James <j.ockenden@yahoo.co.uk>
2024-11-20 16:05:49 +0000

    nout

--- renderer/scripts/slides.js
+++ renderer/scripts/slides.js
@@ -1,19 +1,35 @@
 let slideIndex = 0;
-showSlides();
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  showSlides();
+});
 
 function showSlides() {
-  let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
+  
+  console.log('Total slides:', slides.length);
+  console.log('Total dots:', dots.length);
+
+  for (let i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";  
   }
+  
   slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+
+  for (let i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "inline-block";  
-  dots[slideIndex-1].className += " active";
+
+  if (slides[slideIndex - 1] && dots[slideIndex - 1]) {
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+  } else {
+    console.error('Slide or dot not found at index:', slideIndex - 1);
+  }
+
   setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
